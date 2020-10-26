dcace2d64f73c48eeb437f9bc3254dce02377eac
Eli112358 <Eli112358D@gmail.com>
2020-10-25 23:39:52 -0400

    Add functions to set given or guess

--- src/__init__.py
+++ src/__init__.py
@@ -9,10 +9,20 @@ class Grid:
 		self.size = self.region_size[0] * self.region_size[1]
 		self.cells = np.empty([self.size for _ in range(self.dimensions)], Cell)
 		for cell in np.nditer(self.cells, flags=['refs_ok'], op_flags=['readwrite']):
-			cell[...] = Cell()
+			cell[...] = Cell(self.cells)
 
 
 class Cell(np.object):
 
-	def __init__(self):
-		pass
+	def __init__(self, grid):
+		self.grid = grid
+		self.given = False
+		self.value = None
+
+	def set_given(self, value):
+		self.value = value
+		self.given = value is not None
+
+	def set_guess(self, value):
+		if not self.given:
+			self.value = value
