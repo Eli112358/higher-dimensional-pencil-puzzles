439b3607ad69cf1044d906ead7e09db5f58571f2
Eli112358 <Eli112358D@gmail.com>
2020-10-26 22:12:01 -0400

    Indent with tabs in tests

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -4,22 +4,22 @@ from src.grid import Grid, Cell
 
 
 class TestGrid(unittest.TestCase):
-    d, l, w = 3, 2, 2
-    grid = Grid(d, {0: l, 1: w})
+	d, l, w = 3, 2, 2
+	grid = Grid(d, {0: l, 1: w})
 
-    def test_dimensions(self):
-        self.assertEqual(self.grid.dimensions, self.d)
+	def test_dimensions(self):
+		self.assertEqual(self.grid.dimensions, self.d)
 
-    def test_region_size(self):
-        self.assertEqual(self.grid.region_size[0], self.l)
-        self.assertEqual(self.grid.region_size[1], self.w)
+	def test_region_size(self):
+		self.assertEqual(self.grid.region_size[0], self.l)
+		self.assertEqual(self.grid.region_size[1], self.w)
 
-    def test_size(self):
-        self.assertEqual(self.grid.size, self.l*self.w)
+	def test_size(self):
+		self.assertEqual(self.grid.size, self.l*self.w)
 
-    def test_cell(self):
-        self.assertIsInstance(self.grid.cells[0, 0, 0], Cell)
+	def test_cell(self):
+		self.assertIsInstance(self.grid.cells[0, 0, 0], Cell)
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
