f949a7be34d7fb81b50eb6738a289e0018988eca
Eli112358 <Eli112358D@gmail.com>
2021-01-04 05:06:58 -0500

    Move key list to own file

--- src/game.py
+++ src/game.py
@@ -9,57 +9,16 @@ from pygame.locals import (
 	MOUSEBUTTONDOWN,
 	QUIT,
 	VIDEORESIZE,
-	K_0,
-	K_1,
-	K_2,
-	K_3,
-	K_4,
-	K_5,
-	K_6,
-	K_7,
-	K_8,
-	K_9,
-	K_KP0,
-	K_KP1,
-	K_KP2,
-	K_KP3,
-	K_KP4,
-	K_KP5,
-	K_KP6,
-	K_KP7,
-	K_KP8,
-	K_KP9,
 )
 
 from data import Regioning, Size
 from grid import Grid
+from keys import number_keys
 from rendering import Renderer, Rendering, Colors
 from tuple_util import formula
 
 
 class Game:
-	number_keys = [
-		K_0,
-		K_1,
-		K_2,
-		K_3,
-		K_4,
-		K_5,
-		K_6,
-		K_7,
-		K_8,
-		K_9,
-		K_KP0,
-		K_KP1,
-		K_KP2,
-		K_KP3,
-		K_KP4,
-		K_KP5,
-		K_KP6,
-		K_KP7,
-		K_KP8,
-		K_KP9,
-	]
 
 	def __init__(self, grid: Grid, screen_size: Union[Size, Sequence[int], None]):
 		self.grid = grid
@@ -85,7 +44,7 @@ class Game:
 			self.clear_selection()
 
 	def enter_digit(self, event: Event):
-		digit = self.number_keys.index(event.key) % 10
+		digit = number_keys.index(event.key) % 10
 		selected = [cell[()] for cell in self.plane.iterator() if cell[()].selected]
 		for cell in selected:
 			cell.data.set_guess(digit)
@@ -99,7 +58,7 @@ class Game:
 			if event.type == MOUSEBUTTONDOWN:
 				self.click()
 			if event.type == KEYDOWN:
-				if event.key in self.number_keys:
+				if event.key in number_keys:
 					self.enter_digit(event)
 
 		self.renderer.tick()
