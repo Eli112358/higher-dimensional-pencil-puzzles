ed0db81554fe50748fade7a7c3828326f5d02a8b
Eli112358 <Eli112358D@gmail.com>
2021-02-28 18:34:40 -0500

    Add surface centering

--- src/rendering/graphics.py
+++ src/rendering/graphics.py
@@ -1,8 +1,29 @@
+from enum import Enum
+from typing import Optional
+
 from pygame import (
 	Color,
-	font as fonts,
+	Rect,
 	Surface,
+	font as fonts,
 )
+
+
+class Center(str, Enum):
+	HORIZONTAL = 'top'
+	VERTICAL = 'left'
+
+
+def center(source: Surface, dest: Surface, axes: Optional[Center]) -> Rect:
+	rect = source.get_rect()
+	rect.center = dest.get_rect().center
+	if axes is not None:
+		exec(f'rect.{axes} = source.get_rect().{axes}')
+	return rect
+
+
+def blit_center(source: Surface, dest: Surface, axes: Optional[Center]):
+	dest.blit(source, center(source, dest, axes))
 
 
 def render_text(font: str, text: str, size: int, color: Color) -> Surface:
